ff014d82097241ad2ed4291ce6f3c5576da342ac
Werner Griesel <wernerg@dotslash.co.za>
2020-06-12 18:03:45 +0200

    latest

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,9 +3,19 @@ $(document).ready(function(){
 	$('.navbar-nav a[href="' + document.location.pathname + '"]').addClass('active');
 });
 
-// Parallax images
-var image = document.getElementsByClassName('parallax');
-new simpleParallax(image);
+// Header switch class        
+$(function() {
+    var navheader = $(".navbar");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+	        navheader.addClass("scrolled");
+        } else {
+	        navheader.removeClass("scrolled");
+        }
+    });
+});
 
 // tooltip demo
 $("[data-toggle=tooltip]").tooltip();
@@ -13,16 +23,3 @@ $("[data-toggle=tooltip]").tooltip();
 // popover demo
 $("[data-toggle=popover]").popover();
 
-
-$(document).ready(function () {
-    var $body = $('body');
-    /*------------------------------------------------
-     Sidebar toggle menu
-    -------------------------------------------------*/
-    $body.on('click', '.navigation__sub > a', function (e) {
-        e.preventDefault();
-
-        $(this).parent().toggleClass('navigation__sub--toggled');
-        $(this).next('ul').slideToggle(250);
-    });
-});
